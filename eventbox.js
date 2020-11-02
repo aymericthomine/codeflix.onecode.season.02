d2dccf468a7736ac065f4ee4978fca7de389f640
Aymeric <aymeric@macbook-pro-de-aymeric.home>
2020-11-02 15:13:10 +0100

    Add exo_03

--- eventbox.js
+++ eventbox.js
@@ -1,15 +1,33 @@
+const { EventEmitter } = require('events');
+
 function empty() {
+    const EventEmitter = require('events');
+
+    const emitter = new EventEmitter();
+
+    emitter.on('hi', () => {
+        console.log('CH0ooooooper!');
+    })
+
+    emitter.emit('hi');
+}
+
+function withArgs(names) {
     const EventEmitter = require('events');
 
     const myEmitter = new EventEmitter;
 
-    myEmitter.on('event', () => {
-        console.log('hi');
+    myEmitter.on('newFellow', (name) => {
+        console.log(`Here com's a new pirate ->> ${name}`);
+
     });
-    
-    myEmitter.emit('event');
+
+    for (let i = 0; i < names.length; i++) {
+        myEmitter.emit('newFellow', names[i]);
+    }
 }
 
 module.exports = {
     empty,
+    withArgs,
 }
